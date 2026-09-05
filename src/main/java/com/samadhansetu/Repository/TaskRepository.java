c75cb48996614f6e2c34587eaed7638ba0fc9b52
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:53:48 +0530

    feat: implement task repository

--- src/main/java/com/samadhansetu/Repository/TaskRepository.java
+++ src/main/java/com/samadhansetu/Repository/TaskRepository.java
@@ -1,4 +1,13 @@
 package com.samadhansetu.Repository;
 
-public class TaskRepository {
+import com.samadhansetu.model.entity.Task;
+import com.samadhansetu.model.enums.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByProjectId(Long projectId);
+    List<Task> findByMilestoneId(Long milestoneId);
+    List<Task> findByAssignedToId(Long userId);
+    List<Task> findByStatus(TaskStatus status);
 }
