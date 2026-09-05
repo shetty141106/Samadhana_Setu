8937f887f88ab531a78044835a7a60b41322409b
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:53:44 +0530

    feat: implement project repository

--- src/main/java/com/samadhansetu/Repository/ProjectRepository.java
+++ src/main/java/com/samadhansetu/Repository/ProjectRepository.java
@@ -1,4 +1,11 @@
 package com.samadhansetu.Repository;
 
-public class ProjectRepository {
+import com.samadhansetu.model.entity.Project;
+import com.samadhansetu.model.enums.ProjectStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findByUniversityId(Long universityId);
+    List<Project> findByStatus(ProjectStatus status);
 }
