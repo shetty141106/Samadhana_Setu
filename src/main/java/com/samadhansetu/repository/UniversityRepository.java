dc25164807cab1e73a4d154e2a9750b6c4ac6747
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:03:48 +0530

    Build backend repositories: UniversityRepository

---
+++ src/main/java/com/samadhansetu/repository/UniversityRepository.java
@@ -0,0 +1,13 @@
+package com.samadhansetu.repository;
+
+import com.samadhansetu.model.entity.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface UniversityRepository extends JpaRepository<University, Long> {
+    Optional<University> findByCode(String code);
+    boolean existsByCode(String code);
+    List<University> findByNameContainingIgnoreCase(String name);
+    List<University> findByLocationContainingIgnoreCase(String location);
+}
