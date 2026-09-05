13d9fcdd7f4f0b19ff916255e3c3b32b67b46362
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:01:55 +0530

    Build backend entities: Role

---
+++ src/main/java/com/samadhansetu/model/entity/Role.java
@@ -0,0 +1,22 @@
+package com.samadhansetu.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+}
