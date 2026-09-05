d7dde7d18dde235209a2dc0e1143d547e2bb34bd
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:02:28 +0530

    Build backend entities: Citizen

---
+++ src/main/java/com/samadhansetu/model/entity/Citizen.java
@@ -0,0 +1,12 @@
+package com.samadhansetu.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Citizen {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @OneToOne @JoinColumn(name="user_id", nullable=false, unique=true) private User user;
+    private String phone;
+    private String address;
+}
