4f490bffaa0b275b91312962167f72aa5cef8ec1
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:02:32 +0530

    Build backend entities: NodalOfficer

---
+++ src/main/java/com/samadhansetu/model/entity/NodalOfficer.java
@@ -0,0 +1,11 @@
+package com.samadhansetu.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class NodalOfficer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @OneToOne @JoinColumn(name="user_id", nullable=false, unique=true) private User user;
+    private String assignedArea;
+}
