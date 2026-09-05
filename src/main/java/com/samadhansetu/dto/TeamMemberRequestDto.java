e4253bb535e426d6127dbbfb0942077c2707c82f
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:54:17 +0530

    feat: add team member DTO

---
+++ src/main/java/com/samadhansetu/dto/TeamMemberRequestDto.java
@@ -0,0 +1,4 @@
+package com.samadhansetu.dto;
+import lombok.*;
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class TeamMemberRequestDto { private Long userId; private String memberRole; }
