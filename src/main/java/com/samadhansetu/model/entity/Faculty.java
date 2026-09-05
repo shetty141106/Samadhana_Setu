1dac12ebe4c8d724f471bb6e5f42b0b4e4d3476c
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:02:35 +0530

    Build backend entities: Faculty

---
+++ src/main/java/com/samadhansetu/model/entity/Faculty.java
@@ -0,0 +1,12 @@
+package com.samadhansetu.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Faculty {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @OneToOne @JoinColumn(name="user_id", nullable=false, unique=true) private User user;
+    @ManyToOne private University university;
+    @ManyToOne private Department department;
+}
