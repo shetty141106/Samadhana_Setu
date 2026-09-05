5d477ea1066b0921ef629a05fc53f27db36cd86b
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:02:46 +0530

    Build backend entities: Admin

---
+++ src/main/java/com/samadhansetu/model/entity/Admin.java
@@ -0,0 +1,10 @@
+package com.samadhansetu.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Admin {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @OneToOne @JoinColumn(name="user_id", nullable=false, unique=true) private User user;
+}
