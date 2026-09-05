3686b0ef0e60e70c747f83596de40fec8dfacaa6
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:59:46 +0530

    security: validate issue request input

--- src/main/java/com/samadhansetu/dto/IssueRequestDto.java
+++ src/main/java/com/samadhansetu/dto/IssueRequestDto.java
@@ -1,13 +1,23 @@
 package com.samadhansetu.dto;
 
 import com.samadhansetu.model.enums.IssuePriority;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+
 import java.util.List;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class IssueRequestDto {
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "Description is required")
     private String description;
+
     private String location;
     private String latitude;
     private String longitude;
