6542a4f75b54335728d9b42a4c4461e80417e74f
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:55:20 +0530

    feat: add organization DTO

---
+++ src/main/java/com/samadhansetu/dto/OrganizationRequestDto.java
@@ -0,0 +1,4 @@
+package com.samadhansetu.dto;
+import lombok.*;
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class OrganizationRequestDto { private String name; private String cin; private String gstin; private String udyam; private String address; private String website; }
