5fc7376529469decafe1d1f9cdea088b4b818231
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-05 23:03:00 +0530

    Build backend entities: Department

---
+++ src/main/java/com/samadhansetu/model/entity/Department.java
@@ -0,0 +1,12 @@
+package com.samadhansetu.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Department {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @Column(nullable=false) private String name;
+    private String code;
+    @ManyToOne @JoinColumn(name="university_id", nullable=false) private University university;
+}
