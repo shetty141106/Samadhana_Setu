a9bf7b9bc240ac7cea140eea7c5f3f3a7cdcc25a
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-06 20:38:29 +0530

    test: update JWT service test for configured secret

--- src/test/java/com/samadhansetu/Security/JwtServiceTest.java
+++ src/test/java/com/samadhansetu/Security/JwtServiceTest.java
@@ -8,7 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JwtServiceTest {
 
-    private final JwtService jwtService = new JwtService();
+    private static final String TEST_SECRET = "test-secret-for-jwt-tests-at-least-32-chars";
+    private final JwtService jwtService = new JwtService(TEST_SECRET);
 
     private UserDetails user(String username) {
         return User.withUsername(username).password("password").roles("CITIZEN").build();
