722d99c1f9409012b3fafc53b4ffdd191e673708
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-06 21:11:29 +0530

    security: validate login credentials

--- src/main/java/com/samadhansetu/dto/AuthRequest.java
+++ src/main/java/com/samadhansetu/dto/AuthRequest.java
@@ -1,4 +1,15 @@
 package com.samadhansetu.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class AuthRequest { private String email; private String password; }
+public class AuthRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Enter a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
