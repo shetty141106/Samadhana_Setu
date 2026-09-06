f6704ecf5c437f4ce7cc5b4bd8417240e0253e0b
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-06 23:25:14 +0530

    fix industry repository persistence

--- src/main/java/com/samadhansetu/Repository/IndustryRepository.java
+++ src/main/java/com/samadhansetu/Repository/IndustryRepository.java
@@ -1,4 +1,11 @@
 package com.samadhansetu.Repository;
 
-public class IndustryRepository {
+import com.samadhansetu.model.entity.Industry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IndustryRepository extends JpaRepository<Industry, Long> {
+    Optional<Industry> findByUserId(Long userId);
+    Optional<Industry> findByUserEmail(String email);
 }
