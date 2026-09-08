fa3ba13034d15243058bb6bb1e0a328cba8801c4
Mokshith Shetty <mokshithshetty14@gmail.com>
2026-09-09 00:48:29 +0530

    feat: add admin user creation request

---
+++ src/main/java/com/samadhansetu/dto/AdminCreateUserRequest.java
@@ -0,0 +1,23 @@
+package com.samadhansetu.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class AdminCreateUserRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Enter a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotBlank(message = "Role is required")
+    private String role;
+}
